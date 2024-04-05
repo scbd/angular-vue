e7decb02cd400659cffdc0243a21155a98ab20f3
Stephane Bilodeau <stephane.bilodeau@cbd.int>
2024-04-05 12:53:15 -0400

    Use $attrs wrappers to bridge value to angular (#16)

--- src/components/vue-ng.js
+++ src/components/vue-ng.js
@@ -1,3 +1,4 @@
+import isFunction from 'lodash-es/isFunction';
 import camelCase from 'lodash-es/camelCase';
 import { safeApply } from '../libs/angular-calls';
 import renderVNodeToDomElement from '../libs/render-v-node-to-dom-element';
@@ -11,44 +12,61 @@ export default {
     return h(null, 'angular placeholder');
   },
   mounted () {
-    if (!this.$ngVue) throw new Error('AngularVuePlugin not installed');
+    const { $ngVue, $attrs, $el } = this;
 
-    const { $injector } = this.$ngVue;
+    if (!$ngVue) throw new Error('AngularVuePlugin not installed');
+
+    const { $injector } = $ngVue;
 
     // lookup throughout parent tree first to find closest ngScope.... otherwise
-    const $parentScope = angular.element(this.$el).parents('.ng-scope:first')?.scope() ||
+    const $parentScope = angular.element($el).parents('.ng-scope:first')?.scope() ||
             $injector.get('$rootScope');
 
     const $scope = $parentScope.$new(true); // create new isolated scope!
     this.ngScope = $scope;
 
-    Object.keys(this.$attrs).forEach(attrKey => {
-      const propKey = camelCase(attrKey);
+    const { props, propsSync, events } = parseAttrs($attrs);
 
-      console.debug(`vue(ng): initial set vue => ng (${propKey}):`, this.$attrs[attrKey]);
+    // From Vue => Angular
+    props.forEach(({ attrKey, ngName, vueValue }) => {
+      console.debug(`vue(ng): initial set vue => ng (${ngName}):`, $attrs[attrKey]);
+      // initial set
+      $scope[ngName] = vueValue;
 
-      $scope[propKey] = this.$attrs[attrKey];
-
-      // From Vue => Angular
-      this.$watch(() => this.$attrs[attrKey], (v) => {
+      this.$watch(() => $attrs[attrKey], (v) => {
         if ($scope.$$destroyed) return;
 
         safeApply($scope, () => {
-          if ($scope[propKey] === v) return;
+          if ($scope[ngName] === v) return;
 
-          console.debug(`vue(ng): vue => ng (${propKey})`, v);
-          $scope[propKey] = v;
+          console.debug(`vue(ng): vue => ng (${ngName})`, v);
+          $scope[ngName] = v;
         });
       });
+    });
 
-      // From Angular => Vue
-      $scope.$watch(() => $scope[propKey], (v) => {
-        if (this.$attrs[attrKey] === v) return;
+    // From Angular => Vue (if v-model:)
+    propsSync.forEach(({ attrKey, ngName, vueHandler }) => {
+      console.debug(`vue(ng): two-way vue <=> ng (${ngName}):`);
 
-        console.debug(`vue(ng): ng => vue (${propKey})`, v);
+      $scope.$watch(() => $scope[ngName], (v) => {
+        if ($attrs[attrKey] === v) return;
 
-        this.$emit(`update:${propKey}`, v);
+        console.debug(`vue(ng): vue <= ng (${ngName})`, v);
+
+        vueHandler(v);
       });
+    });
+
+    // From Angular => Vue (v-on:)
+    events.forEach(({ ngName, vueHandler }) => {
+      console.debug(`vue(ng): binding event vue <= ng (${ngName}):`);
+
+      $scope[ngName] = ($event) => {
+        console.debug(`vue(ng): vue <= ng emit:${ngName}`, '$event =', $event);
+
+        vueHandler($event);
+      };
     });
 
     const domElement = renderVNodeToDomElement(this.$slots.default()); // convert default slot to domElement
@@ -73,3 +91,38 @@ export default {
     }
   }
 };
+
+function parseAttrs ($attrs) {
+  const isPropSync = /^onUpdate:/;
+  const isEvent = /^on[A-Z]/;
+
+  let entries = Object.entries($attrs);
+
+  // Starts with are props sync (v-model:) which are all starting with `onUpdate:`
+  const propsSync = entries.filter(([attrKey]) => isPropSync.test(attrKey)).map(([attrKey, vueHandler]) => ({
+    attrKey,
+    ngName: attrKey.replace(isPropSync, ''),
+    vueHandler
+  }));
+
+  entries = entries.filter(([attrKey]) => !propsSync.find(o => o.attrKey === attrKey)); // exclude propsSync;
+
+  // Continue with events which are all starting with `on` and have fandler
+
+  const events = entries.filter(([attrKey, vueHandler]) => isEvent.test(attrKey) && isFunction(vueHandler)).map(([attrKey, vueHandler]) => ({
+    attrKey,
+    ngName: camelCase(attrKey.replace(/^on/, '')),
+    vueHandler
+  }));
+
+  entries = entries.filter(([attrKey]) => !events.find(o => o.attrKey === attrKey)); // exclude events;
+
+  // Remainings are props
+  const props = entries.map(([attrKey, vueValue]) => ({
+    attrKey,
+    ngName: attrKey,
+    vueValue
+  }));
+
+  return { props, propsSync, events };
+}
