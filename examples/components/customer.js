85f3024ee904d27fd73f861cb041a6e0756d42c2
Stephane Bilodeau <stephane.bilodeau@cbd.int>
2024-04-25 10:26:19 -0400

    update examples

--- examples/components/customer.js
+++ examples/components/customer.js
@@ -6,25 +6,24 @@ export default {
   <div>clickCount: {{ clickCount }}</div>
   <div>isOk: {{ isOk }}</div>
   <div>clickCount: <input v-model="clickCount" type="number"></div>
+  <input type="checkbox" v-model="show"> Show
   
-  <h5> vvv this is angular using v-model:click-count="clickCount" vvv</h5>
-  <d-customer v-vue-ng :customer="client" @callback="myAlert($event)" :is-ok="(clickCount%2)!=0" placeholder="my place hoder" v-model:click-count="clickCount"></d-customer>
+  <div v-if="show" >
+    <h5> vvv this is angular using v-model:click-count="clickCount" vvv</h5>
+    <d-customer v-vue-ng.debug :customer="client" @callback="myAlert($event)" :is-ok="(clickCount%2)!=0" placeholder="my place hoder" v-model:click-count="clickCount"></d-customer>
 
-  <h5> vvv this is angular "&lt;div v-vue-ng:d-customer.a .../&gt;" vvv</h5>
-  <div v-vue-ng:d-customer.a :customer="client" @callback="myAlert($event)" :is-ok="(clickCount%2)!=0" :click-count="clickCount"></div>
-
-  <h5> vvv this is angular "&lt;div v-vue-ng:d-customer.c .../&gt;" vvv</h5>
-  <div v-vue-ng:d-customer.c :customer="client" @callback="myAlert($event)" :is-ok="(clickCount%2)!=0" :click-count="clickCount"></div>
-
-  <h5> vvv this is angular "&lt;div v-vue-ng:d-customer .../&gt;" vvv</h5>
-  <div v-vue-ng:d-customer :customer="client" @callback="myAlert($event)" :is-ok="(clickCount%2)!=0" :click-count="clickCount"></div>
-
-
+    <h5> vvv this is angular "&lt;div v-vue-ng:d-customer .../&gt;" vvv</h5>
+    <div v-vue-ng:d-customer :customer="client" @callback="myAlert($event)" :is-ok="(clickCount%2)!=0" :click-count="clickCount"></div>
+  
+    <h5> vvv prevent warning! this is angular "&lt;ng v-vue-ng:d-customer .../&gt;" vvv </h5>
+    <ng v-vue-ng:d-customer :customer="client" @callback="myAlert($event)" :is-ok="(clickCount%2)!=0" v-model:click-count="clickCount"></ng>
+  </div>
 </div>
 `,
 
 
     data: () => ({
+        show: true,
         clickCount : 0,
         client: {
             firstName : 'my',
